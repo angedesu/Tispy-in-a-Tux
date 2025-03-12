539dcab5b56eb43b7f6e2fa1f00686f5a67ba567
huyvu203 <168234998+huyvu203@users.noreply.github.com>
2025-03-12 02:56:25 -0700

    Added Start Screen <-> Home Screen navigation (no animation yet)

---
+++ Assets/Scipts/ChangeScene.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene: MonoBehaviour
+{
+    public void GoToHomeScreen()
+    {
+        SceneManager.LoadScene("Home Screen");
+    }
+
+    public void GoToStartScreen()
+    {
+        SceneManager.LoadScene("Start Screen");
+    }
+}
