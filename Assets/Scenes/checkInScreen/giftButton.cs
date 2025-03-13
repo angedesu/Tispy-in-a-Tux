96e348f2607037306992a871d81676163d8bd5fc
angedesu <104692309+angedesu@users.noreply.github.com>
2025-03-13 12:40:44 -0700

    finalized check in screen

--- Assets/Scenes/checkInScreen/giftButton.cs
+++ Assets/Scenes/checkInScreen/giftButton.cs
@@ -1,22 +1,23 @@
 using System;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 public class giftButton : MonoBehaviour
 {
     public GameObject claimText;
     public GameObject claimedText;
-    private string lastClaim = "Claim1";
-    private string lastLogin = "Login1";
-    
+    public GameObject claimedGift;
+    public GameObject streakNumber;
     
     private void Start()
     {
-        CheckStatus();
+        // PlayerPrefs.DeleteAll();
+        CheckGiftStatus();
         UpdateLogin();
+        CheckStreakNumber();
     }
-
-    void CheckStatus()
+    void CheckGiftStatus()
     {
         string lastClaimDate = PlayerPrefs.GetString("lastClaim", "");
         string today = DateTime.Today.ToString("MM/dd/yyyy");
@@ -27,31 +28,77 @@ public class giftButton : MonoBehaviour
             this.gameObject.SetActive(false);
             claimText.SetActive(false);
             claimedText.SetActive(true);
+            claimedGift.SetActive(true);
         }
         else
         {
-            // display gift and do NOT display claimed text 
+            // have not claimed, display gift and do NOT display claimed text 
             this.gameObject.SetActive(true);
             claimText.SetActive(true);
             claimedText.SetActive(false);
+            claimedGift.SetActive(false);
         }
     }
 
+    void CheckStreakNumber()
+    { 
+        string lastStreak = PlayerPrefs.GetString("streakNumber", "0");
+        
+        streakNumber.GetComponent<TMP_Text>().text = lastStreak;
+    }
     void UpdateLogin()
     {
         string today = DateTime.Today.ToString("MM/dd/yyyy");
         PlayerPrefs.SetString("lastLogin", today);
         PlayerPrefs.Save();
     }
-    public void OnMouseDown()
+    public void OnClick()
     {
-        // since clicked on gift, save todays date and update
+        // since clicked on gift, save today's date and update
         string today = DateTime.Today.ToString("MM/dd/yyyy");
         PlayerPrefs.SetString("lastClaim", today);
         PlayerPrefs.Save();
         // Stop displaying gift and show claimed text
         this.gameObject.SetActive(false);
         claimText.SetActive(false);
-        claimText.SetActive(false);
+        claimedText.SetActive(true);
+        claimedGift.SetActive(true);
+        
+        CheckLogin();
+    }
+    void CheckLogin()
+    {
+        string LastLogin = PlayerPrefs.GetString("lastLogin", "");
+        DateTime today = DateTime.Today;
+        // string today = DateTime.Today.ToString("MM/dd/yyyy");
+        // DateTime todayDate = DateTime.Parse(today);
+        
+        // subtract today and last login ()
+        TimeSpan span = today - DateTime.Parse(LastLogin);
+        int days = span.Days;
+        
+        if (days > 1)
+        {
+            // more than 1 day => lost streak, reset streak
+            streakNumber.GetComponent<TMP_Text>().text = "1";
+        }
+        else if (days == 1)
+        {
+            // one day apart => streak intact, increment streak by 1
+            streakNumber.GetComponent<TMP_Text>().text += "1";
+        }
+        else
+        {
+            // reset counter to 1, 
+            streakNumber.GetComponent<TMP_Text>().text = "1";
+        }
+        UpdateStreakNumber();
+    }
+
+    void UpdateStreakNumber()
+    {
+        string number = streakNumber.GetComponent<TMP_Text>().text;
+        PlayerPrefs.SetString("streakNumber", number);
+        PlayerPrefs.Save();
     }
 }
