bf02d9cbc1bba9e006e6aff5a827302ea8f8138f
TheSassyMarquess <tristantalesmichealmovies@gmail.com>
2025-03-19 01:23:39 -0700

    I figured out how to dynamically create objects based off a list, correctly aligned everything, and got it all working on dummy data. Now it just needs connecting to the database and the LIST portion is done

--- Assets/Scenes/Leaderboard/LeaderboardFetch.cs
+++ Assets/Scenes/Leaderboard/LeaderboardFetch.cs
@@ -10,6 +10,14 @@ namespace Leaderboard
     {
         public struct LeaderboardEntry
         {
+            public LeaderboardEntry(string u = "", int s = 0)
+            {
+                user = u;
+                score = s;
+            }
+            //WHY DO STRUCTS HAVE CONSTRUCTORS. THEY AREN'T OBJECTS. WHY IS C# MAKING THEM CLASSES
+            //WHY EVEN HAVE STRUCTS
+            //C# is evidence God is dead. Microsoft is evil. Who makes a struct a class?!
             public string user;
             public int score;
             //I'm looking up if I need to do operator overload or something else to get these sorted correctly
@@ -18,22 +26,27 @@ namespace Leaderboard
         public GameObject playerRow;
         public Text rowText;
         public Transform leaderboardRow;
-        public async void Start()
+        public void Start()
         {
+            Debug.Log("Leaderboard Script loaded");
             //Run code on scene loading
             //Set up the leaderboard
-            await this.Fetch();
+            this.Fetch();
             this.SortLeaderboard();
             //Create objects for each item in the list
             //Populate each objects text with the getLeaderboardEntry function
             Vector3 position = leaderboardRow.position;
             foreach (LeaderboardEntry player in ranking)
             {
+                Debug.Log("In Loop. Player is " + player.user);
                 //Instatiate object
                 rowText.text = player.user+ " " + player.score + " wins";
-                position.y -= 90;
-                leaderboardRow.position = position;
-                Instantiate(playerRow, leaderboardRow);
+                GameObject tmp = Instantiate(playerRow, leaderboardRow);
+                tmp.transform.position = position;
+                //I have no clue why unity is offsetting these so much
+                //It should be
+                //position.y -= 90;
+                position.y -= 2.5f;
             }
         }
         public string getLeaderboardEntry(int index)
@@ -51,7 +64,7 @@ namespace Leaderboard
             //Create a new leaderboard
             Leaderboard newBoard = new Leaderboard();
             //Setup the leaderboard
-            await newBoard.Fetch();
+            newBoard.Fetch();
             newBoard.SortLeaderboard();
             /*This is my best attempt at a async constructor
             *Hopefully it doesn't have a memory leak or anything
@@ -61,19 +74,19 @@ namespace Leaderboard
             return newBoard;
         }
         //Fetch for Leaderboard
-        private async Task Fetch()
+        private void Fetch()
         {
+            Debug.Log("Leaderboard Fetch called");
             //Dummy debug code
             //*
             string[] players = { "Alice", "Bob", "Charlie", "Dan", "Evan" };
             int[] wins = { 50, 40, 30, 10, 5 };
             for (int i = 0; i < 5; i++)
             {
-                LeaderboardEntry profile = new LeaderboardEntry
-                {
-                    score = wins[i],
-                    user = players[i]
-                };
+                LeaderboardEntry profile = new LeaderboardEntry(players[i], wins[i]);
+
+                ranking.Add(profile);
+                Debug.Log("Creating Profile: " + profile.user + " " + profile.score);
             }
             //*/
             //Connect to the database
@@ -82,10 +95,11 @@ namespace Leaderboard
         }
         private void SortLeaderboard()
         {
+            Debug.Log("Leaderboard Sort Called");
             //Sort the entire leaderboard
             ranking.Sort(delegate (LeaderboardEntry player1, LeaderboardEntry player2)
             {
-                return player1.score.CompareTo(player2.score);
+                return player2.score.CompareTo(player1.score);
             });
         }
     }
