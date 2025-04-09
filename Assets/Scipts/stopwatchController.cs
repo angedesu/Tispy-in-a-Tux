a0915e726ea77fb3846565537cb399f385a6ebcb
Daphne Rios <daphnerios1@gmail.com>
2025-04-09 00:57:31 -0700

    Update PouringTimer

--- Assets/Scipts/stopwatchController.cs
+++ Assets/Scipts/stopwatchController.cs
@@ -1,22 +1,53 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class stopWatch : MonoBehaviour
+public class stopWatchController : MonoBehaviour, IPointerDownHandler
 {
     [SerializeField] TextMeshProUGUI stopwatchText;
     private float elapsedTime;
     private bool isRunning = false;
     private bool isPaused = false;
 
+    private RectTransform stopwatchRect;
+    private Vector2 stemAreaCenter = new Vector2(0.5f, 0.15f);
+    private float stemAreaRadius = 0.1f;
+
+    void Start() {
+        stopwatchRect = GetComponent<RectTransform>();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-
         if (isRunning && !isPaused)
         {
             UpdateTime();
+        }
+    }
+
+    //Handles Clicks
+    public void OnPointerDown(PointerEventData eventData) {
+        // normalize pointer position & then normalize the coordinates
+        Vector2 localPoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(stopwatchRect, eventData.position, eventData.enterEventCamera, out localPoint);
+        Vector2 normalizedPoint = new Vector2((localPoint.x / stopwatchRect.rect.width) + 0.5f, (localPoint.y / stopwatchRect.rect.height) + 0.5f);
+
+        if (Vector2.Distance(normalizedPoint, stemAreaCenter) <= stemAreaRadius) // click in stem area, reset timer
+        {
+            ResetStopWatch();
+        }
+        else // click in the rest of the stopwatch area
+        { 
+            if (isRunning)
+            {
+                PauseStopWatch();
+            }
+            else {
+                StartStopWatch();
+            }
         }
     }
 
