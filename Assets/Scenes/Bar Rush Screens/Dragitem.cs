1c386e43d04884cabff14cc666af1c1853257d0b
huyvu203 <168234998+huyvu203@users.noreply.github.com>
2025-04-24 20:21:05 -0700

    added .idea/ to git ignore

--- Assets/Scenes/Bar Rush Screens/Dragitem.cs
+++ Assets/Scenes/Bar Rush Screens/Dragitem.cs
@@ -4,21 +4,32 @@ using UnityEngine.EventSystems;
 public class DragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private Transform originalParent;
-    
-    
-    
+    private RectTransform rectTransform;
+    private Canvas canvas;
+
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+
+    }
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
         originalParent = transform.parent;
-        transform.SetParent(transform.root);
-        transform.SetAsLastSibling();
+        transform.SetParent(canvas.transform); // Set parent to Canvas
+        transform.SetAsLastSibling(); // Set on top
     }
 
-    public void OnDrag(PointerEventData eventData)
+    public void OnDrag(PointerEventData eventData)    
     {
         Debug.Log("OnDrag");
-        transform.position = Input.mousePosition;
+        Vector2 localPoint;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), eventData.position, canvas.worldCamera, out localPoint))
+        {
+            rectTransform.anchoredPosition = localPoint;
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
