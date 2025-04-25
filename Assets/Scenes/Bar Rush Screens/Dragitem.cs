dc87d0330e55ea542b3efb65e8fd1e4ae130af33
huyvu203 <168234998+huyvu203@users.noreply.github.com>
2025-04-25 03:08:17 -0700

    dragging item done

--- Assets/Scenes/Bar Rush Screens/Dragitem.cs
+++ Assets/Scenes/Bar Rush Screens/Dragitem.cs
@@ -1,25 +1,45 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private Transform originalParent;
     private RectTransform rectTransform;
     private Canvas canvas;
+    private Vector2 dragOffset;
+    private Image itemImage;
+    private Color originalColor;
+    private float dragAlpha = 0.7f;
+    
 
 
     private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
         canvas = GetComponentInParent<Canvas>();
-
+        
+        itemImage = GetComponent<Image>();
+        originalColor = itemImage.color;
+        
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
         originalParent = transform.parent;
+        
         transform.SetParent(canvas.transform); // Set parent to Canvas
         transform.SetAsLastSibling(); // Set on top
+        
+        // Calculate drag offset
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), eventData.position, canvas.worldCamera, out Vector2 mouseLocalPosition);
+        dragOffset = rectTransform.anchoredPosition - mouseLocalPosition;
+        
+        // Turn item semi transparent
+        Color tempColor = originalColor;
+        tempColor.a = dragAlpha;
+        itemImage.color = tempColor;
+
     }
 
     public void OnDrag(PointerEventData eventData)    
@@ -28,7 +48,7 @@ public class DragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
         Vector2 localPoint;
         if (RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), eventData.position, canvas.worldCamera, out localPoint))
         {
-            rectTransform.anchoredPosition = localPoint;
+            rectTransform.anchoredPosition = localPoint + dragOffset;
         }
     }
 
@@ -36,5 +56,8 @@ public class DragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
     {
         Debug.Log("OnEndDrag");
         transform.SetParent(originalParent);
+        
+        // Restore item original alpha
+        itemImage.color = originalColor;
     }
 }
