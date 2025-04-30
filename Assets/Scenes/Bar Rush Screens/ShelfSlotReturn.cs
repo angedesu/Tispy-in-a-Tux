afc142fa211fc3241f2b9244ed9bd4154c9826a4
huyvu203 <168234998+huyvu203@users.noreply.github.com>
2025-04-30 10:04:01 -0700

    fixed recipe book

---
+++ Assets/Scenes/Bar Rush Screens/ShelfSlotReturn.cs
@@ -0,0 +1,11 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShelfSlotReturn : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
